309a93ca8c63be5850e45b8c0a521f00d03223f9
John Drab <john.drab@yahoo.com>
2025-03-19 17:41:27 -0700

    Update test_script_ExtMem.sh
    Adding description comments to test file

--- ExtMem_Tests/test_script_ExtMem.sh
+++ ExtMem_Tests/test_script_ExtMem.sh
@@ -1,10 +1,12 @@
-export DRAMSIZE=134217728000   
+export DRAMSIZE=134217728000
 export SWAPDIR=/dev/sda99  
 #export SWAPDIR=/
-export LD_PRELOAD=/users/jpdrab/ExtMem/src/libextmem-default.so  
+export LD_PRELOAD=/users/jpdrab/ExtMem/src/libextmem-default.so  #Replace with your file path
+
+#Put command to run the desired program below:
 
 #./memory_test
-#stress --vm 4 --vm-bytes 25G --timeout 30s
-./file_processer file1.dat file2.dat file3.dat
+stress --vm 4 --vm-bytes 25G --timeout 30s
+#./file_processer file1.dat file2.dat file3.dat
 #./bench_markDC
 
